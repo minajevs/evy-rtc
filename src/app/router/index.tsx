3841a5f9081f85b2ced04923c32de9bc43286531
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2020-04-03 22:31:26 +0300

    whatabout hashrouter

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 export type Props = {}
 
 export const RouterOutlet: React.FC<Props> = ({ children }) => {
     return (
         // needs /evy because hosted on gh-pages
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
             {children}
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
